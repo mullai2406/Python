bb3ed62c832413a6f8fb8e587629115bb7e92729
mullai2406 <135659554+mullai2406@users.noreply.github.com>
2024-11-07 08:06:13 +0530

    Add files via upload

---
+++ Extra_Assig_Leve2.py
@@ -0,0 +1,36 @@
+print("HOPE AI")
+
+sname = input("Enter Your Institute name : ")
+print(sname)
+
+a=20
+b=3
+c=a+b
+print("Add : "+str(c))
+
+
+c=a-b
+print("Sub : "+str(c))
+
+
+c=a*b
+print("Multiple : "+str(c))
+
+
+c=a/b
+print("Div : "+str(c))
+
+
+c=a//b
+print("Floor : "+str(c))
+
+c=a%b
+print("Mod : "+str(c))
+
+c=a**b
+print("Power : "+str(c))
+
+
+
+
+
